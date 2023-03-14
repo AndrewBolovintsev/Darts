849ca09136d5b43818f5abf705db145a2459ce32
AndrewBolovintsev <basoo2512@gmail.com>
2023-03-14 15:49:16 +0300

    The end

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ ball.addEventListener('mousemove', event => {
     const cursor = ball.offsetWidth / 2;
     ball.style='left: ' + (ballPosition - cursor) + 'px';
     ball.addEventListener('click', () => {
-        ball.style='top: 45px; left: ' + (ballPosition - cursor) + 'px; width: 25px; height: 25px;';
+        ball.style='top: 45px; left: ' + (ballPosition - cursor) + 'px; width: 20px; height: 20px;';
         const ballImage = document.querySelector('.ball-image');
-        ballImage.style='top: 45px; left: ' + (ballPosition - cursor) + 'px; width: 25px; height: 25px;';
+        ballImage.style='top: 45px; left: ' + (ballPosition - cursor) + 'px; width: 20px; height: 20px;';
         console.log(ball.style.left);
-        if (ball.style.left >= '670px' && ball.style.left <= '770px' && ball.style.top == '45px') {
+        if (ball.style.left >= '690px' && ball.style.left <= '750px' && ball.style.top == '45px') {
             popalText.style='opacity: 1;';
         }
     })
