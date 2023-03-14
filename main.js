6b4f4afb511efe016b905938186d0c2b920fb34b
AndrewBolovintsev <basoo2512@gmail.com>
2023-03-14 13:48:36 +0300

    14 March

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 document.getElementsByClassName('bal')[0].style= "left:" + Math.random() * (1400 - 0) + 0 + "px";
 const ball = document.getElementsByClassName('bal')[0];
+const popalText = document.querySelector('.popados');
+
 ball.addEventListener('mousemove', event => {
     const ballPosition = event.clientX;
     const cursor = ball.offsetWidth / 2;
     ball.style='left: ' + (ballPosition - cursor) + 'px';
+    ball.addEventListener('click', () => {
+        ball.style='top: 45px; left: ' + (ballPosition - cursor) + 'px; width: 25px; height: 25px;';
+        const ballImage = document.querySelector('.ball-image');
+        ballImage.style='top: 45px; left: ' + (ballPosition - cursor) + 'px; width: 25px; height: 25px;';
+        console.log(ball.style.left);
+        if (ball.style.left >= '670px' && ball.style.left <= '770px' && ball.style.top == '45px') {
+            popalText.style='opacity: 1;';
+        }
+    })
 })
-
-ball.addEventListener('mouseout', event => {
-    const ballOutPosition = event.clientX;
-    const cursor = ball.offsetWidth / 2;
-    ball.style='left: ' + (ballOutPosition - cursor) + 'px';
-})
-
-ball.addEventListener('click', event => {
-    const windowHeight = document.querySelector('body').clientHeight;
-    console.log(windowHeight);
-    ball.style='top: -' + windowHeight + 'px';
-})
-
-document.getElementById("imaaaga").innerHTML = '<img src="ball.png" width="50" height="50" />';
